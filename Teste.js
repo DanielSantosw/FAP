a7bcf18ac651382c93c6ef4acfef17a1a4e404f3
DanielSantosw <142314558+DanielSantosw@users.noreply.github.com>
2023-08-17 08:42:23 -0300

    Update Teste.js

---
+++ Teste.js
@@ -0,0 +1,6 @@
+var nome = prompt ("Informe seu nome: ")
+var idade = prompt ("Informe sua idade: ")
+
+alert('Olá' + nome + ' sua idade é : ' + idade)
+console.log('nome: ' + nome);
+console.log('idade: ' + idade);
